d2794cfdebedd8dc5711a58154e6075671a3bb87
SUPATKN <yacht1400@gmail.com>
2023-01-03 16:31:52 +0700

    lab11_3 c

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<fstream>
+#include<cstdlib>
+#include<string>
+
+using namespace std;
+
+int main(){
+    double mean;
+    double sumX2;
+    double sum;
+    int count;
+    string text;
+    ifstream source("score.txt");
+    while(getline(source,text)){
+        sum += atof(text.c_str());
+        sumX2 += pow(atof(text.c_str()),2);
+        count++;
+    }
+    mean = sum/count;
+    cout << "Number of data = "<< count  << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = "<< sqrt((sumX2/count)-pow(mean,2));
+}
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
